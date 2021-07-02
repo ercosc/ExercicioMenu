977b4b82d5efe09f5328c488d864a290882e9f93
Erick Rennan Coelho <ercosc@gmail.com>
2021-07-01 21:00:27 -0300

    adicionando comentarios

--- ExercicioMenu/Program.cs
+++ ExercicioMenu/Program.cs
@@ -5,9 +5,11 @@ namespace ExercicioMenu
 {
     class Program
     {
+        //lista que recebe todos os nomes
         static List<String> nomes = new List<string>();
         static void Main(string[] args)
         {
+            //inicia com o menu
             Menu();
         }
 
@@ -20,6 +22,7 @@ namespace ExercicioMenu
 
 
         }
+        //mostra as opções para o usuario
         private static void TxtMenu()
         {
             Console.WriteLine("Menu Crud de nomes:");
@@ -30,6 +33,7 @@ namespace ExercicioMenu
             Console.WriteLine("5 - Sair:");
         }
 
+        // recebe a opção que o usuario escolheu e encaminha para seus respectivos menus
         private static void InputMenu(string input)
         {
             switch (input)
@@ -68,28 +72,33 @@ namespace ExercicioMenu
         private static void AdicionarNome()
         {
             Console.WriteLine("digite o nome para adicionar:");
+            //recebe o nome do usuario
             string nome = Console.ReadLine();
-
+            //verifica se esse nome ja existe
             if (!VerificarNome(nome))
             {
+                // caso exista verifica se é espaços em branco e tal
                 if (string.IsNullOrWhiteSpace(nome))
                 {
                     Console.WriteLine("formato de nome inválido\n");
                 }
                 else
                 {
+                    //caso esteja tudo certo o nome é adicionado
                     nomes.Add(nome);
                     Console.WriteLine("nome adicionado com sucesso\n");
                 }
             }
             else
             {
+                //caso o nome ja exista
                 Console.WriteLine($"O nome {nome} já existe.\n");
             }
         }
-
+        //lista todos os nomes
         private static void ListarNomes()
         {
+            //verifica se a lista está vazia para listar
             if (nomes.Count == 0)
             {
                 Console.WriteLine("Insira algum nome na lista.\n");
@@ -108,14 +117,17 @@ namespace ExercicioMenu
         {
             Console.WriteLine("Digite o nome que deseja alterar na lista:");
             string nomePAlterar = Console.ReadLine();
+            //verifica se aquele nome existe na lista
             if (nomes.Contains(nomePAlterar))
             {
                 for (int i = 0; i < nomes.Count; i++)
                 {
+                    //encontra o nome que deseja alterar
                     if (nomes[i] == nomePAlterar)
                     {
                         Console.WriteLine("novo nome:");
                             string novoNome = Console.ReadLine();
+                        //verifica se o novo nome não existe na lista
                         if(nomes.Contains(novoNome))
                         {
                             Console.WriteLine("Nome já cadastrado.\n");
@@ -134,6 +146,7 @@ namespace ExercicioMenu
 
         }
 
+        //verifica se o nome existe na lista
         private static bool VerificarNome(string nome)
         {
             foreach (string n in nomes)
@@ -145,15 +158,18 @@ namespace ExercicioMenu
             }
             return false;
         }
+
         private static void DeletarNome()
         {
             Console.WriteLine("digite o nome que deseja deletar:");
             string nome = Console.ReadLine();
+            //verifica se o nome existe na lista
             if (VerificarNome(nome))
             {
                 for (int i = 0; i < nomes.Count; i++)
                 {
                     {
+                        //encontra o nome na lista e deleta
                         if (nomes[i] == nome)
                         {
                             nomes.RemoveRange(i, 1);
